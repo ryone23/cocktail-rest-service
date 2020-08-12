08bc728a74f4b969690a785378ccf5ee8c635a30
Ronald Yonemura <Yone@Yones-Macbook.local>
2020-08-12 11:05:24 -0700

    Add project files

---
+++ cocktail_service/src/main/java/com/cocktailbar/cocktail_service/CocktailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cocktailbar.cocktail_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CocktailServiceApplication.class, args);
+    }
+
+}
---
+++ cocktail_service/src/test/java/com/cocktailbar/cocktail_service/CocktailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocktailbar.cocktail_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
